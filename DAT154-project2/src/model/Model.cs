ec3cfa8c383ce971ed4bf3f763baead4c70a441e
Lars Magnus Nordeide <600871@stud.hvl.no>
2023-02-27 14:24:15 +0100

    Model

---
+++ DAT154-project2/src/model/Model.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace DAT154_project2.model;
+
+public class Model
+{
+    private List<StellarObject> objects;
+
+    public Model(string filename)
+    {
+        
+    }
+}
---
+++ DAT154-project2/src/model/StellarObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography;
+
+namespace DAT154_project2.model;
+
+public abstract class StellarObject
+{
+    public string name;
+    public StellarObject parent;
+
+    public double OrbitDistance;
+    public double OrbitalPeriod;
+
+    public void render()
+    {
+        
+    }
+
+}
