d6c4fb7c0cd41cecd41a412efc1767ab07e35099
Lars Magnus Nordeide <600871@stud.hvl.no>
2023-03-06 00:51:02 +0100

    Refactor Model Object Structure

---
+++ SpaceModel/src/model/Entity.cs
@@ -0,0 +1,39 @@
+﻿using no.hvl.DAT154.V23.GROUP14.SpaceModel.graphics;
+
+namespace no.hvl.DAT154.V23.GROUP14.SpaceModel;
+
+public class Entity {
+    protected readonly string name;
+    protected double orbital_period;
+
+    protected double orbital_radius;
+    protected Entity parent;
+
+    public Entity(string name, double orbitalRadius, double orbitalPeriod) {
+        this.name = name;
+        this.orbital_radius = orbitalRadius;
+        this.orbital_period = orbitalPeriod;
+    }
+
+    public void render(GraphicsAPI graphics, long time) {
+        Vector center = new() {
+            x = double.Sin(2.0 * double.Pi * time / orbital_period) * orbital_radius,
+            y = double.Cos(2.0 * double.Pi * time / orbital_period) * orbital_radius,
+            z = 0.0
+        };
+
+        Properties properties = new() {
+            fillColor = new Color { r = 255, g = 255, b = 255 }
+        };
+
+        // graphics.drawCircle(center, radius, properties);
+    }
+
+    public void setParent(Entity parent) {
+        this.parent = parent;
+    }
+
+    public string getName() {
+        return name;
+    }
+}
--- SpaceModel/src/model/Model.cs
+++ SpaceModel/src/model/Model.cs
@@ -2,15 +2,14 @@
 using System.Diagnostics;
 using Microsoft.VisualBasic.FileIO;
 using no.hvl.DAT154.V23.GROUP14.SpaceModel.graphics;
-using no.hvl.DAT154.V23.GROUP14.SpaceModel.model;
 
 namespace no.hvl.DAT154.V23.GROUP14.SpaceModel;
 
 public class Model {
-    private readonly Dictionary<string, StellarBody> objects;
+    private readonly Dictionary<string, Entity> objects;
 
     public Model() {
-        objects = new Dictionary<string, StellarBody>();
+        objects = new Dictionary<string, Entity>();
     }
 
     public static Model LoadFromFile(string filename) {
@@ -19,9 +18,6 @@ public class Model {
         TextFieldParser parser = new(filename);
         parser.TextFieldType = FieldType.Delimited;
         parser.SetDelimiters(";");
-
-        var ring_system_counter = 0;
-        RingSystem? ring_system = null;
 
         while (!parser.EndOfData) {
             string[] row = parser.ReadFields();
@@ -32,15 +28,10 @@ public class Model {
             if (string.Equals(name, "Name")) 
                 continue;
 
-            if (string.IsNullOrEmpty(name)) {
-                Debug.WriteLine("\nnew ring system:");
-                ring_system = new RingSystem(ring_system_counter.ToString());
-                model.addObject(ring_system);
-                ring_system_counter++;
+            if (string.IsNullOrEmpty(name)) 
                 continue;
-            }
 
-            StellarBody parent = model.findObjectByName(row[1]);
+            Entity parent = model.findObjectByName(row[1]);
 
             double orbital_radius;
             if (row[2].Equals("-"))
@@ -54,38 +45,36 @@ public class Model {
             else
                 orbital_period = Convert.ToDouble(row[3]);
 
-            SphericalBody sb = new(name, orbital_radius, orbital_period);
-            sb.setParent(parent);
+            Entity entity = new(name, orbital_radius, orbital_period);
+            entity.setParent(parent);
 
-            if (!model.addObject(sb)) throw new InvalidOperationException("Possible duplicate in csv file! Could not parse the file!");
-
-            if (ring_system != null) ring_system.addRingObject(sb);
-
-
+            if (!model.addObject(entity)) 
+                throw new InvalidOperationException("Possible duplicate in csv file! Could not parse the file!");
+            
             Debug.WriteLine(row[0]);
         }
 
         return model;
     }
 
-    public bool addObject(StellarBody body) {
-        return objects.TryAdd(body.getName(), body);
+    public bool addObject(Entity entity) {
+        return objects.TryAdd(entity.getName(), entity);
     }
 
-    public bool removeObject(StellarBody body) {
-        return objects.Remove(body.getName());
+    public bool removeObject(Entity entity) {
+        return objects.Remove(entity.getName());
     }
 
-    public StellarBody? findObjectByName(string name) {
-        return objects.TryGetValue(name, out StellarBody? value) ? value : null;
+    public Entity? findObjectByName(string name) {
+        return objects.TryGetValue(name, out Entity? entity) ? entity : null;
     }
 
-    public void render(GraphicsAPI graphics, long time, Action<StellarBody, Exception>? onException) {
-        foreach (StellarBody body in objects)
+    public void render(GraphicsAPI graphics, long time, Action<Entity, Exception>? onException) {
+        foreach (Entity entity in objects)
             try {
-                body.render(graphics, time);
+                entity.render(graphics, time);
             } catch (Exception e) {
-                onException?.Invoke(body, e);
+                onException?.Invoke(entity, e);
             }
     }
 }
