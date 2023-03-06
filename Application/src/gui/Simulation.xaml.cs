7ac55a0a4768a7890a825dcce658e36d87f4a7c1
Lars Magnus Nordeide <600871@stud.hvl.no>
2023-03-06 15:01:32 +0100

    Add Simulation Rendering
    TODO: Add color and size
    TODO: Add transform

--- Application/src/gui/Simulation.xaml.cs
+++ Application/src/gui/Simulation.xaml.cs
@@ -1,26 +1,90 @@
 ﻿using System;
+using System.Numerics;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 using System.Windows.Threading;
 using no.hvl.DAT154.V23.GROUP14.SpaceModel;
+using Type = no.hvl.DAT154.V23.GROUP14.SpaceModel.Type;
 
-namespace DAT154_project2.gui; 
+namespace DAT154_project2.gui;
 
-public partial class Simulation : UserControl {
+public partial class Simulation : Canvas {
+    private readonly Model model;
+    private float time;
+    private readonly DispatcherTimer timer;
 
-    private Model model;
-    private DispatcherTimer timer;
-    
     public Simulation() {
         InitializeComponent();
-        
+
         // TODO: Instantiate model
+        model = Model.LoadFromFile("Planets.csv");
+
+        /*model = new Model();
+        model.addObject(new Entity("Star") {
+            type = Type.STAR
+        });
+        model.addObject(new Entity("Planet") {
+            type = Type.GASGIANT,
+            orbit = new Orbit {
+                origin = model.findObjectByName("star"),
+                distance = 150.0f,
+                period = 10.0f
+            }
+        });
+        model.addObject(new Entity("Moon") {
+            type = Type.TERRESTIAL,
+            orbit = new Orbit {
+                origin = model.findObjectByName("Planet"),
+                distance = 50.0f,
+                period = 1.0f
+            }
+        });*/
 
         timer = new DispatcherTimer();
-        timer.Tick += Render;
+        timer.Interval = TimeSpan.FromSeconds(1.0 / 60.0);
+        timer.Tick += OnTick;
+        timer.Start();
     }
 
-    private void Render(object? sender, EventArgs e) {
-        // TODO: Render model
-        throw new NotImplementedException();
+    private void OnTick(object? sender, EventArgs e) {
+        //time += DateTime.Now.Ticks / (float) TimeSpan.TicksPerSecond - time;
+        time += 365.0f * 24.0f * 1.0f / 60.0f;
+
+        model.OnTick(time);
+
+        InvalidateVisual();
+    }
+
+    protected override void OnRender(DrawingContext dc) {
+        base.OnRender(dc);
+
+        dc.PushTransform(new TranslateTransform(ActualWidth * 0.5, ActualHeight * 0.5));
+
+        /*Vector3 pos = model.findObjectByName("Star").position;
+        dc.DrawEllipse(Brushes.Red, null, new Point(pos.X, pos.Y), 50.0, 50.0);
+        
+        pos = model.findObjectByName("Planet").position;
+        dc.DrawEllipse(Brushes.Red, null, new Point(pos.X, pos.Y), 30.0, 30.0);*/
+
+        model.ForEach(
+            entity => {
+                Vector3 pos = entity.position;
+
+                switch (entity.type) {
+                    case Type.STAR:
+                        dc.DrawEllipse(Brushes.Yellow, null, new Point(pos.X, pos.Y), 50.0, 50.0);
+                        break;
+                    case Type.GASGIANT:
+                        dc.DrawEllipse(Brushes.Green, null, new Point(pos.X, pos.Y), 30.0, 30.0);
+                        break;
+                    case Type.TERRESTIAL:
+                        dc.DrawEllipse(Brushes.Gray, null, new Point(pos.X, pos.Y), 10.0, 10.0);
+                        break;
+                    default:
+                        dc.DrawEllipse(Brushes.Gray, null, new Point(pos.X, pos.Y), 2.0, 2.0);
+                        break;
+                }
+            });
     }
 }
--- SpaceModel/src/model/Entity.cs
+++ SpaceModel/src/model/Entity.cs
@@ -9,7 +9,9 @@ public class Entity {
     private double cacheTime;
 
     public Orbit? orbit;
-    private Vector3 _position;
+    private Vector3 _position = Vector3.Zero;
+
+    public Type? type;
 
     public Vector3 position {
         get => getPosition();
@@ -33,19 +35,19 @@ public class Entity {
 
     private Vector3 getPosition() {
         if (model == null)
-            return _position;
+            return Vector3.Zero;
 
         if (cacheTime == model.time)
             return _position;
         cacheTime = model.time;
         
-        if (orbit == null) 
+        if (!(orbit is Orbit o)) 
             return _position;
-        Orbit o = orbit.Value;
 
-        float theta = 2.0f * float.Pi * model.time / o.period;
-        
-        _position = (o.origin?.position ?? Vector3.Zero) + new Vector3{ X = o.distance * float.Sin(theta), Y = o.distance * float.Cos(theta), Z = 0.0f };
+        float theta = (o.period > 0.0) ? (2.0f * float.Pi * model.time / o.period) : 0.0f;
+
+        _position = (o.origin?.getPosition() ?? Vector3.Zero);
+        _position += new Vector3{ X = float.Sqrt(o.distance + 1.0f) * float.Sin(theta), Y = float.Sqrt(o.distance + 1.0f) * float.Cos(theta), Z = 0.0f } / 4.0f;
 
         return _position;
     }
@@ -55,4 +57,10 @@ public struct Orbit {
     public Entity origin;
     public float distance;
     public float period;
+}
+
+public enum Type {
+    STAR,
+    GASGIANT,
+    TERRESTIAL,
 }
