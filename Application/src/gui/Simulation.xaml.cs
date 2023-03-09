8269b611d523e01719598efb27c997bc699b4084
Lars Magnus Nordeide <600871@stud.hvl.no>
2023-03-09 13:10:18 +0100

    Rafactor Rendering

--- Application/src/gui/Simulation.xaml.cs
+++ Application/src/gui/Simulation.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Numerics;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Threading;
 using no.hvl.DAT154.V23.GROUP14.SpaceModel;
+using Type = no.hvl.DAT154.V23.GROUP14.SpaceModel.Type;
 
 namespace DAT154_project2.gui; 
 
@@ -24,7 +26,6 @@ public partial class Simulation : Canvas {
     }
 
     private void OnTick(object? sender, EventArgs e) {
-        //time += DateTime.Now.Ticks / (float) TimeSpan.TicksPerSecond - time;
         time += 365.0f * 24.0f * 1.0f / 60.0f;
 
         model.OnTick(time);
@@ -37,25 +38,24 @@ public partial class Simulation : Canvas {
 
         dc.PushTransform(new TranslateTransform(ActualWidth * 0.5, ActualHeight * 0.5));
 
-        /*Vector3 pos = model.findObjectByName("Star").position;
-        dc.DrawEllipse(Brushes.Red, null, new Point(pos.X, pos.Y), 50.0, 50.0);
-        
-        pos = model.findObjectByName("Planet").position;
-        dc.DrawEllipse(Brushes.Red, null, new Point(pos.X, pos.Y), 30.0, 30.0);*/
-
         model.ForEach(
             entity => {
-                Vector3 pos = entity.position;
-
+                Vector3 pos = entity.getRelativePosition();
+                pos *= (entity.orbit?.index + 1.0f ?? 1.0f) / pos.Length();
+                pos += entity.orbit?.origin.getAbsolutePosition() ?? Vector3.Zero;
+                
                 switch (entity.type) {
                     case Type.STAR:
-                        dc.DrawEllipse(Brushes.Yellow, null, new Point(pos.X, pos.Y), 50.0, 50.0);
+                        dc.DrawEllipse(Brushes.Yellow, null, new Point(pos.X, pos.Y), 6.0, 6.0);
                         break;
-                    case Type.GASGIANT:
-                        dc.DrawEllipse(Brushes.Green, null, new Point(pos.X, pos.Y), 30.0, 30.0);
+                    case Type.GAS:
+                        dc.DrawEllipse(Brushes.Green, null, new Point(pos.X, pos.Y), 5.0, 5.0);
                         break;
-                    case Type.TERRESTIAL:
-                        dc.DrawEllipse(Brushes.Gray, null, new Point(pos.X, pos.Y), 10.0, 10.0);
+                    case Type.ROCK:
+                        dc.DrawEllipse(Brushes.Gray, null, new Point(pos.X, pos.Y), 4.0, 4.0);
+                        break;
+                    case Type.ICE:
+                        dc.DrawEllipse(Brushes.Blue, null, new Point(pos.X, pos.Y), 3.0, 3.0);
                         break;
                     default:
                         dc.DrawEllipse(Brushes.Gray, null, new Point(pos.X, pos.Y), 2.0, 2.0);
