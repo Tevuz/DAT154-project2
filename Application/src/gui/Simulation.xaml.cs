dbab9100911cda141a60ee7cf6c2fc4fd5ef8bd5
Lars Magnus Nordeide <600871@stud.hvl.no>
2023-03-10 23:57:19 +0100

    Add Mouse Move

--- Application/src/gui/Simulation.xaml.cs
+++ Application/src/gui/Simulation.xaml.cs
@@ -6,29 +6,33 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Threading;
 using no.hvl.DAT154.V23.GROUP14.SpaceModel;
+using Vector = System.Windows.Vector;
 
 namespace DAT154_project2.gui; 
 
 public partial class Simulation : Canvas {
+    
     private readonly Model model;
     private float time;
-    private readonly DispatcherTimer timer;
 
-    private float x, y;
+    private Vector2 mouse;
+    private Vector3 view;
 
     public Simulation() {
         InitializeComponent();
         
         model = Model.LoadFromFile("res/Planets.csv");
 
-        timer = new DispatcherTimer();
+        DispatcherTimer timer = new();
         timer.Interval = TimeSpan.FromSeconds(1.0 / 60.0);
         timer.Tick += OnTick;
         timer.Start();
+
+        view = Vector3.UnitZ;
     }
 
     private void OnTick(object? sender, EventArgs e) {
-        time += 24f * 1.0f / 60.0f;
+        time += 3600f * 1.0f / 60.0f;
 
         InvalidateVisual();
     }
@@ -37,7 +41,7 @@ public partial class Simulation : Canvas {
         base.OnRender(dc);
 
         dc.PushTransform(new TranslateTransform(ActualWidth * 0.5, ActualHeight * 0.5));
-        dc.PushTransform(new TranslateTransform(x, y));
+        dc.PushTransform(new TranslateTransform(view.X, view.Y));
 
         model.ForEach(
             entity => {
@@ -78,12 +82,19 @@ public partial class Simulation : Canvas {
             });
     }
 
-    private void MouseMoveEvent(object sender, MouseEventArgs e) {
-        if (e.LeftButton == MouseButtonState.Pressed) {
-            this.x += (float) e.GetPosition(null).X;
-            this.y += (float) e.GetPosition(null).Y;
-        }
-            
+    public void Handler_MouseDown(object sender, MouseEventArgs e) {
+        Point point = e.GetPosition(null);
+        mouse = new Vector2((float) point.X, (float) point.Y);
+    }
+
+    private void Handler_MouseMoved(object sender, MouseEventArgs e) {
+        if (e.LeftButton != MouseButtonState.Pressed)
+            return;
+
+        Point point = e.GetPosition(null);
+        Vector2 next = new((float) point.X, (float) point.Y);
+        view += new Vector3((next - mouse) * view.Z, 0.0f);
+        mouse = next;
     }
 
 }
